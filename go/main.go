70d66493f48a5df59480931567330fc77d65c5d0
fx <fx@win22.corp>
2021-08-06 15:12:05 +0800

    ADD: port/dir config from both env/cli

--- go/main.go
+++ go/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
 
 func main() {
 	fmt.Println("go working!")
+
+	// loop log
+	go func() {
+		n, tick := 0, 0
+		for {
+			n++
+			time.Sleep(5 * time.Second)
+			if (n < 100) || (n%12 == 0) {
+				tick++
+				log.Printf("tick=%d n=%d", tick, n)
+			}
+		}
+	}()
+
+	// web server
+	addr, dir := ":8080", "/www"
+	// get config from env
+	if v, ok := os.LookupEnv("WEB_HTTP_ADDR"); ok {
+		addr = v
+	}
+	if v, ok := os.LookupEnv("WEB_HTTP_DIR"); ok {
+		dir = v
+	}
+	// from cli
+	if len(os.Args) > 1 {
+		addr = os.Args[1]
+		if len(os.Args) > 2 {
+			dir = os.Args[2]
+		}
+	}
+
+	log.Printf("web server started in dir=%s at addr=%s", dir, addr)
+	log.Fatal(http.ListenAndServe(addr, http.FileServer(http.Dir(dir))))
 }
---
+++ go/main_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	t.Logf("ok ok, test ok")
+}
